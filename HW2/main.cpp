ae0c9db066c9ef628c575d71c83a313dea5533de
wcchang1019 <destiny10191019@gmail.com>
2018-10-22 02:58:08 +0800

    add makefile

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -4,42 +4,37 @@
 #include <iomanip>
 #include <fstream>
 #include <time.h>
+#include <omp.h>
+#include <math.h>
+#include <sstream>
 
 using namespace std;
 clock_t total = clock();
 
-bool classifySubset(vector<int> test,vector<vector<int>> all)
+bool classifySubset(vector<int> test,vector<vector<int> > all)
 {
 	for(int i=0;i<test.size();i++){
         vector<int> new_arr = test;
         new_arr.erase(new_arr.begin()+i);
-        int j=0;
-        for(j=0;j<all.size();j++){
-            if(all[j] == new_arr){
-                break;
-            }
-        }
-        if(j == all.size()){
+        if(find(all.begin(), all.end(), new_arr) == all.end()){
             return false;
         }
 	}
 	return true;
 }
 
-vector<vector<int>> getCandidate(vector<vector<int>> L){
-    vector<vector<int>> ans;
-    for(int i = 0; i < int(L.size()); i++){
-        for(int j = i + 1; j < int(L.size()); j++){
+vector<vector<int> > getCandidate(vector<vector<int> > L){
+    vector<vector<int> > ans;
+    vector< vector<int> >::iterator row, rowadd;
+    int len = L[0].size();
+    for(row = L.begin(); row != L.end(); ++row){
+        for(rowadd = row + 1; rowadd < L.end(); rowadd++){
             vector<int> uni;
-            set_union(L[i].begin(), L[i].end(), L[j].begin(), L[j].end(), back_inserter(uni));
-            if(uni.size() == L[j].size()+1){
-                int k = 0;
-                for(k = 0; k < ans.size(); k++){
-                   if(uni == ans[k]){
-                        break;
-                   }
-                }
-                if(k == ans.size()){
+            set_union(row->begin(), row->end(), rowadd->begin(), rowadd->end(), back_inserter(uni));
+            if(uni.size() == len+1){
+                if(find(ans.begin(), ans.end(), uni) != ans.end()){
+                    break;
+                }else{
                     if(classifySubset(uni, L)){
                         ans.push_back(uni);
                     }
@@ -50,9 +45,9 @@ vector<vector<int>> getCandidate(vector<vector<int>> L){
     return ans;
 }
 
-pair<vector<pair<vector<int>,int>>, vector<vector<int>>> countCandidate(vector<vector<int>> transactions, vector<vector<int>> candidate, int threshold){
-    vector<pair<vector<int>,int>> votedPair;
-    vector<vector<int>> nextCandidate;
+pair<vector<pair<vector<int>,int> >, vector<vector<int> > > countCandidate(vector<vector<int> > transactions, vector<vector<int> > candidate, int threshold){
+    vector<pair<vector<int>,int> > votedPair;
+    vector<vector<int> > nextCandidate;
     for(int i = 0;i < candidate.size(); i++){
         int cnt = 0;
         for(int j = 0;j < transactions.size(); j++){
@@ -71,13 +66,19 @@ pair<vector<pair<vector<int>,int>>, vector<vector<int>>> countCandidate(vector<v
     return make_pair(votedPair,nextCandidate);
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
     clock_t tStart = clock();
-    float min_support = 0.1;
-    //vector<pair<vector<int>,int>> selected_arr;
-    freopen("sample2.in","r", stdin);
-    vector<vector<int>> transactions;
+    istringstream aaaa(argv[1]);
+    float min_support;
+    aaaa >> min_support;
+    FILE *fPtr;
+    fPtr = freopen(argv[2], "r", stdin);
+    if (!fPtr) {
+        printf("open file failed");
+        exit(1);
+    }
+    vector<vector<int> > transactions;
     string line;
     while(!getline(cin, line).eof()){
         vector<int> arr;
@@ -89,52 +90,44 @@ int main(void)
         transactions.push_back(arr);
     }
     int threshold = min_support*int(transactions.size());
-    /*for(int i=0;i<1000;i++){
-        if(count_arr[i] >= threshold){
-                vector<int> vec;
-                vec.push_back(i);
-                selected_arr.push_back(make_pair(vec, count_arr[i]));
-                Lone.push_back(vec);
-        }
-    }*/
-    vector<vector<int>> Lone;
+    vector<vector<int> > Lone;
     fstream fp;
-    fp.open("test.txt", ios::out | ios::trunc);
+    fp.open(argv[3], ios::out | ios::trunc);
     for(int i=0;i<1000;i++){
         vector<int> vec;
         vec.push_back(i);
         Lone.push_back(vec);
     }
     for(;;){
-        pair<vector<pair<vector<int>,int>>, vector<vector<int>>> test;
+        pair<vector<pair<vector<int>,int> >, vector<vector<int> > > test;
         clock_t tStart = clock();
         test = countCandidate(transactions, Lone, threshold);
-        printf("countCandidate: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+        //printf("countCandidate: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
         tStart = clock();
-        for(auto x: test.first){
-            /*for(auto y: x.first){
-                cout << y ;
-            }*/
-            for(int yy=0;yy<x.first.size();yy++){
-                fp << x.first[yy];
-                if(yy!=x.first.size()-1){
+        vector<pair<vector<int>,int> >::iterator x;
+        for(x=test.first.begin();x<test.first.end();++x){
+        //for(auto x: test.first){
+            for(int yy=0;yy<x->first.size();yy++){
+                fp << x->first[yy];
+                if(yy!=x->first.size()-1){
                     fp << ",";
                 }
             }
+            float ans = floor(((float)x->second*10000/(float)transactions.size()) + 0.5) / 10000.;
             fp << ":";
-            fp << setprecision (4) << fixed << (float)x.second/(float)transactions.size() << endl;
+            fp << setprecision (4) << fixed << ans << endl;
         }
-        printf("outputtime: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+        //printf("outputtime: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
         Lone.clear();
         tStart = clock();
         Lone = getCandidate(test.second);
         if(Lone.size() == 0){
             break;
         }
-        printf("getCandidate: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+        //printf("getCandidate: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     }
     fp.close();
-    printf("total: %.2fs\n", (double)(clock() - total)/CLOCKS_PER_SEC);
+    //printf("total: %.2fs\n", (double)(clock() - total)/CLOCKS_PER_SEC);
 
 return 0;
 }
